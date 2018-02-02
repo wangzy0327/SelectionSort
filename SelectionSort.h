b8af6f61123be4a533a9c7092bf8003645d2a15a
wangzy0327 <wangzy0327@qq.com>
2018-02-02 16:11:11 +0800

    optimize insertion sort

--- SelectionSort.h
+++ SelectionSort.h
@@ -8,16 +8,17 @@
 #include <iostream>
 #include <string>
 
-template <typename T>
-void selectionSort(T arr[],int n){
-    for(int i = 0;i < n;i++){
+template<typename T>
+void selectionSort(T arr[], int n) {
+    for (int i = 0; i < n; i++) {
         //寻找[i,n)区间中的最小值
         int minIndex = i;
-        for(int j = i + 1;j < n;j++)
-            if(arr[j] < arr[minIndex])
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[minIndex])
                 minIndex = j;
         //swap 在iostream中  algorithm中也有
-        swap(arr[i],arr[minIndex]);
+        swap(arr[i], arr[minIndex]);
     }
 }
+
 #endif //SELECTIONSORT_SELECTIONSORT_H
