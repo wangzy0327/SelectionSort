41f7da40c434761cee69b017f5274d2224c37aa6
wangzy0327 <wangzy0327@qq.com>
2018-02-05 21:36:30 +0800

    revise debug main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "Student.h"
 #include "BubbleSort.h"
 #include "MergeSort.h"
+#include "QuickSort.h"
 
 using namespace std;
 
@@ -39,11 +40,11 @@ using namespace std;
 //        cout<<d[i];
 //    cout<<endl;
 int main() {
-    int n = 50000;
-//    int *arr = SortTestHelper::generateRandomArray(n,0,n);
+    int n = 100000;
+    int *arr = SortTestHelper::generateRandomArray(n,0,n);
 //    int *arr2 = So rtTestHelper::copyIntArray(arr,n);
     //只有两百个元素不在他本来应该在的位置
-    int *arr = SortTestHelper::generateNearlyOrderedArray(n, 1000);
+//    int *arr = SortTestHelper::generateNearlyOrderedArray(n, 1000);
     int *arr2 = SortTestHelper::copyIntArray(arr, n);
     int *arr3 = SortTestHelper::copyIntArray(arr, n);
     int *arr4 = SortTestHelper::copyIntArray(arr, n);
@@ -54,7 +55,7 @@ int main() {
     SortTestHelper::testSort("Selection Sort", selectionSort, arr, n);
     SortTestHelper::testSort("Insertion Sort", insertionSort, arr2, n);
     SortTestHelper::testSort("Merge Sort from Up to Bottom", mergeSortUB, arr3, n);
-    SortTestHelper::testSort("Merge Sort from Bottom to Up", mergeSortUB, arr4, n);
+    SortTestHelper::testSort("Merge Sort from Bottom to Up", mergeSortBU, arr4, n);
     SortTestHelper::testSort("Bubble Sort", bubbleSort, arr5, n);
 
     //释放内存
@@ -62,6 +63,7 @@ int main() {
     delete[] arr2;
     delete[] arr3;
     delete[] arr4;
+    delete[] arr5;
 
     return 0;
 }
